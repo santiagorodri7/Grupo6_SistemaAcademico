37f7be9ce0e213350727ad380cd31284466bad60
Sala K 301 <salak301@salasfr.itm.edu.co>
2026-08-14 10:46:07 -0500

    Agrego el encapsulamiento

--- model/domain/Estudiante.java
+++ model/domain/Estudiante.java
@@ -1,14 +1,22 @@
 public class Estudiante{
-    String nombreC;
-    int edad ;
-    String identificacion;
-    String carrera;
+    private String nombreC;
+    private int edad ;
+    private String identificacion;
+    private String carrera;
     public Estudiante(String nombreC, int edad, String identificacion,String carrera){
         this.nombreC=nombreC;
         this.edad=edad;
         this.identificacion=identificacion;
         this.carrera=carrera;
     }
+    //get
+    public String getNombre(){
+        return this.nombreC;
+    }     
+    //set
+    public String setNombre(){
+        return this.nombreC;
+    } 
     public void mostrarDatos(){
         System.out.printIn("Nombre Estudiante: "+nombreC+" tengo "+edad+" y estudio "+carrera)
     }
