1e14cd6dc257e938e9bbc4cbaff0c02da0df4d81
desarrollo <desarrollo@academicos.cesde.edu.co>
2026-08-13 20:06:55 -0500

    correccion del nombre del metodo mostrarinfo

--- Profesor.java
+++ Profesor.java
@@ -11,7 +11,7 @@ public Profesor(String nombre, String asignatura, String documento, int edad){
     this.documento = documento;
     this.edad = edad;
 }
-public void mostratInfo(){
+public void mostrarInfo(){
     System.out.println("Hola, soy el profesor " + this.nombre + ", doy la asignatura de: " + asignatura + " y mi documento es " + documento);
 
 }
