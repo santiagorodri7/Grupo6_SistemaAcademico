1bf75135adaf813f2ab6d91af34a3397d2e14b8e
vasil <vasil.zabuza.luffy@gmail.com>
2026-08-25 20:53:23 -0500

    Sube las clases Estudiante, Matricula, Materia, Calificacion

---
+++ model/domain/Calificacion.java
@@ -0,0 +1,61 @@
+public class Calificacion {
+
+    private double notaParcial1;
+    private double notaParcial2;
+    private double notaFinal;
+    private String observaciones;
+
+    private Materia materia;//Esto asocia a calificacion con materia
+                           //creo que no lo vimos en clase 
+    public Calificacion() {
+    }
+
+    public Calificacion(double notaParcial1, double notaParcial2,
+                         double notaFinal, String observaciones, Materia materia) {
+        this.notaParcial1 = notaParcial1;
+        this.notaParcial2 = notaParcial2;
+        this.notaFinal = notaFinal;
+        this.observaciones = observaciones;
+        this.materia = materia;
+    }
+
+    public double getNotaParcial1() {
+        return notaParcial1;
+    }
+
+    public void setNotaParcial1(double notaParcial1) {
+        this.notaParcial1 = notaParcial1;
+    }
+
+    public double getNotaParcial2() {
+        return notaParcial2;
+    }
+
+    public void setNotaParcial2(double notaParcial2) {
+        this.notaParcial2 = notaParcial2;
+    }
+
+    public double getNotaFinal() {
+        return notaFinal;
+    }
+
+    public void setNotaFinal(double notaFinal) {
+        this.notaFinal = notaFinal;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+}
