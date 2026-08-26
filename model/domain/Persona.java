758435df8023dc7fddd1da16099ebcfe34607cbe
Sala K 304 <salak304@salasfr.itm.edu.co>
2026-08-26 11:16:42 -0500

    agregacion de interfaz y de la clase abstracta persona, adicionalmente solucion de varios errores en el codigo

---
+++ model/domain/Persona.java
@@ -0,0 +1,44 @@
+public abstract class Persona implements RolAcademico {
+
+    private String identificacion;
+    private String nombre;
+    private String correo;
+
+    public Persona(String nombre, String correo, String identificacion) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.identificacion = identificacion;
+    }
+
+    public String datosPersona() {
+        return "Mi identificacion es " + identificacion
+                + ", mi nombre es " + nombre
+                + " y mi correo es " + correo;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public abstract String identificarRol();
+}
