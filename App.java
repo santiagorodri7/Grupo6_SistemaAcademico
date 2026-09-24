012e45df2d55f3aab4fde225bcb524b9ac0afe40
santiago <elbecxy@gmail.com>
2026-09-24 13:01:32 -0500

    Simplificacion de todo el codigo que habia en App

--- App.java
+++ App.java
@@ -1,15 +1,7 @@
+import view.MenuView;
+
 public class App {
-    public static void main(String[] args) {
-
-        Estudiante estudiante = new Estudiante("Ana Torres", "ana@correo.com", "1001", "E-100", 3);
-
-        Profesor profesor = new Profesor("Carlos Ruiz", "carlos@correo.com", "2002", "P-200", "Sistemas");
-        
-        System.out.println(estudiante.identificarRol());
-        System.out.println(estudiante.datosResumen());
-
-        System.out.println(profesor.identificarRol());
-        System.out.println(profesor.datosResumen());       
-
+    public static void main(String[] args) throws Exception {
+        new MenuView().iniciar();
     }
 }
